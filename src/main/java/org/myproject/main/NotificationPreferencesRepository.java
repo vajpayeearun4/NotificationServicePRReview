cbd2fedb93836de55e7a82a2f281d581d3954e81
Arun Bajpai <vajpayeearun4@gmail.com>
2025-08-06 12:19:58 +0530

    added new files

--- src/main/java/org/myproject/main/NotificationPreferencesRepository.java
+++
@@ -1,8 +0,0 @@
-package org.myproject.main;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationPreferencesRepository extends JpaRepository<NotificationPreferencesEntity, String> {
-}
--- src/main/java/org/myproject/notificationpreferenceservice/NotificationPreferenceRepository.java
+++
@@ -1,8 +0,0 @@
-package org.myproject.notificationpreferenceservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationPreferenceRepository extends JpaRepository<NotificationPreferencesEntity, String> {
-}
